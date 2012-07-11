3e442ccd199fb1a4de508c1c46be9e774fcd6c23
brandonjmc@gmail.com <brandonjmc@gmail.com@6e892773-7e6c-67d2-7534-3dd42dd14f95>
2012-07-11 18:45:08 +0000

    added code to update the database based on error clicked in MarkWordActivity

--- src/com/cybertrons/orfa/MarkWordActivity.java
+++ src/com/cybertrons/orfa/MarkWordActivity.java
@@ -1,32 +1,90 @@
 package com.cybertrons.orfa;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MarkWordActivity extends Activity {
-public final static String NUMBER = "cs4953.advsoft.orfa.MESSAGE";
+	
+	public final static String NUMBER = "cs4953.advsoft.orfa.MESSAGE";
+	private static final int REQUEST_CODE = 99;
+	
+	public int uidWord;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+		// Get the message from the intent
+		Intent intent = getIntent();
+		uidWord = intent.getIntExtra(DisplayButtonsActivity.WORD, 0);
         setContentView(R.layout.word_mark);
         
         
     }
 	
-	public void onClickErrorOmit(View view){
+	public void onClickErrorOmit(View view){		
+		DataBaseHelper dbHelper = new DataBaseHelper(this);	
+		try {
+			dbHelper.openDataBase();
+		} catch (SQLException sqle) {
+			throw sqle;
+		}
+		try {
+			dbHelper.setCurrentSessionError(uidWord, 1);
+		} catch (SQLException sqle) {
+			throw sqle;
+		}
+		finish();
+	}
+	
+	public void onClickErrorWordOrder(View view){		
+		DataBaseHelper dbHelper = new DataBaseHelper(this);	
+		try {
+			dbHelper.openDataBase();
+		} catch (SQLException sqle) {
+			throw sqle;
+		}
+		try {
+			dbHelper.setCurrentSessionError(uidWord, 2);
+		} catch (SQLException sqle) {
+			throw sqle;
+		}
 		finish();
     }
 	
-	public void onClickErrorWordOrder(View view){
+	public void onClickErrorMispronounce(View view){		
+		DataBaseHelper dbHelper = new DataBaseHelper(this);	
+		try {
+			dbHelper.openDataBase();
+		} catch (SQLException sqle) {
+			throw sqle;
+		}
+		try {
+			dbHelper.setCurrentSessionError(uidWord, 3);
+		} catch (SQLException sqle) {
+			throw sqle;
+		}
 		finish();
     }
 	
-	public void onClickErrorMispronounce(View view){
-		finish();
-    }
-	
-	public void onClickErrorHesitate(View view){
+	public void onClickErrorHesitate(View view){		
+		DataBaseHelper dbHelper = new DataBaseHelper(this);	
+		try {
+			dbHelper.openDataBase();
+		} catch (SQLException sqle) {
+			throw sqle;
+		}
+		try {
+			dbHelper.setCurrentSessionError(uidWord, 4);
+		} catch (SQLException sqle) {
+			throw sqle;
+		}
 		finish();
     }
 	
