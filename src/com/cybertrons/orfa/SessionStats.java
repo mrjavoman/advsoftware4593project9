0f2581beeafae7466db8ec0026b25af9fa886e3f
lizglz9 <lizglz9@6e892773-7e6c-67d2-7534-3dd42dd14f95>
2012-07-16 20:28:45 +0000

    updated the AndroidManifest.xml with the landscape orientation

--- src/com/cybertrons/orfa/SessionStats.java
+++ src/com/cybertrons/orfa/SessionStats.java
@@ -21,7 +21,7 @@ public class SessionStats extends Activity {
 		// Get the message from the intent
 		Intent intent = getIntent();
 		lastWord = intent.getIntExtra(MarkWordActivity.UID_WORD, 0);
-	    //setContentView(R.layout.word_mark);
+	  
 		   //setContentView(R.layout.word_mark_cell); //runs with cell screen
 		setContentView(R.layout.word_mark_tablet); //runs with tablet screen
 		   
