be47f9256348df1d5536411810d849c396d85731
mrjavoman@hotmail.com <mrjavoman@hotmail.com@6e892773-7e6c-67d2-7534-3dd42dd14f95>
2012-07-16 20:10:46 +0000

    added a class for writing a csv file and added some code to implement that class in DBTesterActivity

--- branches/Orfa/src/com/cybertrons/orfa/DbTesterActivity.java
+++ branches/Orfa/src/com/cybertrons/orfa/DbTesterActivity.java
@@ -5,51 +5,32 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.View;
 
 public class DbTesterActivity extends Activity {
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.db_tester);
-
-		
-		//Beginning Arthur's clock
-		final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-		Runnable runnable = new Runnable() {
-
-			public void run() {
-
-				builder.setMessage(
-						"Time is up! Please select the last word read!")
-						.setCancelable(true)
-						.setPositiveButton("Yes",
-								new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog,
-											int which) {
-										// TODO Auto-generated method stub
-
-									}
-								})
-						.setNegativeButton("No",
-								new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog,
-											int id) {
-										dialog.cancel();
-									}
-								});
-				AlertDialog alert = builder.create();
-				alert.show();
-			}
-		};
-
-		final Handler h = new Handler();
-		long time = 10000; //This is the amount of time in milliseconds to delay before saying "stop"
-		h.postDelayed(runnable, time);
-		//end arthur's clock
-		
-		
-	
 	}
+	
+	public void onClickExportCSV(View view)  {
+
+		DataBaseHelper dbHelper = new DataBaseHelper(this);
+		try  {
+			dbHelper.createDataBase();
+			dbHelper.openDataBase();
+			dbHelper.writeCSV();
+			dbHelper.close();
+		}
+		catch(Exception ex)  {
+			Log.e("Error in DbTesterActivity",ex.toString());
+		}
+
+	}
+	
+	
 }
