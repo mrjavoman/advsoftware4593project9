53114673864b35e50d6cedbdc82c17bfa9d39db5
brandonjmc@gmail.com <brandonjmc@gmail.com@6e892773-7e6c-67d2-7534-3dd42dd14f95>
2012-07-25 22:55:46 +0000

    fixed an error in the last method.  Save session "committ" button works now.

--- src/com/cybertrons/orfa/DataBaseHelper.java
+++ src/com/cybertrons/orfa/DataBaseHelper.java
@@ -494,16 +494,7 @@ public class DataBaseHelper extends SQLiteOpenHelper{
 		public void writeSession(int finalErrors, int finalScore, String finalNotes) {
 			int student = 0;
 			
-			String aSql = "SELECT name"
-					+" FROM current_session"
-					+" GROUP BY name";
-	 
-			Cursor aCursor = myDataBase.rawQuery(aSql, null);
-	        aCursor.moveToFirst();
-	        student = aCursor.getInt(0);
-	        aCursor.close();
-	        
-	        myDataBase.execSQL("INSERT INTO session (notes, id_student, incorrect_count, score)"
+			myDataBase.execSQL("INSERT INTO session (notes, id_student, incorrect_count, score)"
 	        	+" VALUES ( " + finalNotes + ", "+ student + ", "+ finalErrors  + ", "+ finalScore+ ")");
 			
 		}
