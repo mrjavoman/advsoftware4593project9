79a5ef59c6ac79ca86f51f93dc1a186ad9e59046
arthur.garzajr <arthur.garzajr@6e892773-7e6c-67d2-7534-3dd42dd14f95>
2012-07-26 21:55:43 +0000

    Small but necessary update.  You no longer have to toggle the settings checkbox to initialize the option to show the "time is up!" message.  It's default value is to show the message.  The user can select the option to not show again.
    Brandon knows what I'm talking about.  It was small and almost unnoticeable.

--- branches/Orfa/src/com/cybertrons/orfa/MainActivity.java
+++ branches/Orfa/src/com/cybertrons/orfa/MainActivity.java
@@ -3,17 +3,32 @@ package com.cybertrons.orfa;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.View;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 	
 	public final static String NUMBER = "cs4953.advsoft.orfa.MESSAGE";
-	
+	public static final String PREFS_NAME = "orfa_settings";
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // If a Preference called "skipMessage" isn't created in the app, create it
+        // and set it to "NOT checked".  This means that the "Do not show this message"
+        // check box is "NOT checked".
+        final SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        if(!settings.contains("skipMessage")){
+        	Log.i("debugger", "Initializing the skipMessage SharedPreference");
+        	SharedPreferences.Editor editor = settings.edit();
+        	// Initializing "skipMessage" to "NOT checked"
+            editor.putString("skipMessage", "NOT checked");
+            // Committing initialization.
+            editor.commit();
+        }
     }
     
     public void onClickStartSession(View view){
