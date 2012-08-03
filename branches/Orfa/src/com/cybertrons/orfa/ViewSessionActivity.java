77f4d2e8c878aa16b305cd7c4e7c2527ca2a236a
mrjavoman@hotmail.com <mrjavoman@hotmail.com@6e892773-7e6c-67d2-7534-3dd42dd14f95>
2012-08-03 00:33:22 +0000

    added two classes for session list view

---
+++ branches/Orfa/src/com/cybertrons/orfa/ViewSessionActivity.java
@@ -0,0 +1,63 @@
+package com.cybertrons.orfa;
+
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ViewSessionActivity extends ListActivity {
+	
+	private static String[] data = new String[] { "0", "1", "2", "3", "4", "5" };
+	
+	private SessionCursorAdapter dataSource;
+	
+	private DataBaseHelper dbHelper = new DataBaseHelper(this);
+	
+	private int student_id;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.view_session);
+        
+        Intent intent = getIntent();
+        student_id = intent.getIntExtra(StudentCursorAdapter.getStudentId(), 0);
+               
+        Cursor cursorData = dbHelper.getSession(student_id);
+        dataSource = new SessionCursorAdapter(this, cursorData);
+        
+        setListAdapter(dataSource);
+       
+        
+    }
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+      // TODO Auto-generated method stub
+      super.onListItemClick(l, v, position, id);
+      Toast.makeText(this, "Click-" + String.valueOf(position), Toast.LENGTH_SHORT).show();
+    }
+
+}
