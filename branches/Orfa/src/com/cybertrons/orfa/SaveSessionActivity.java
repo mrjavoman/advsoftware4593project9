9e5fdc48ec27dba7dc8b349c96de429a4001fa1f
brandonjmc@gmail.com <brandonjmc@gmail.com@6e892773-7e6c-67d2-7534-3dd42dd14f95>
2012-08-11 18:26:45 +0000

    Fixed save session single quote bug.  Also removed select story and select student from the back stack

--- branches/Orfa/src/com/cybertrons/orfa/SaveSessionActivity.java
+++ branches/Orfa/src/com/cybertrons/orfa/SaveSessionActivity.java
@@ -52,9 +52,9 @@ public class SaveSessionActivity extends Activity {
 	   	}catch(SQLException sqle){
 	   		throw sqle;
 	   	}
-	    /*TODO
-	     * still need to handle when the user enter's a single quote in the text.  Have to escape the 
-	     */
+	    
+	    this.finalNotes = this.finalNotes.replace("'", "''");
+	    
 	    try {
 			dbHelper.writeSession(finalErrors, finalScore, finalNotes);
 	   	}catch(SQLException sqle){
